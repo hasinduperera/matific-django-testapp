db491d39ed11d92556ba454f3caeba112a6fa5d9
Hasindu Perera <hasindu.perera@syscolabs.com>
2023-02-19 11:50:32 +0530

    1. Adding ECR/ECS and ALB resources 2. Mapping ci-cd workflow file with ECR/ECS resources

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -2,7 +2,13 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import { IpAddresses } from 'aws-cdk-lib/aws-ec2';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+
+// all resources should be tagged accordingly
+
+// also I would concider creating different .ts files for each resource sets for better management
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -100,5 +106,69 @@ export class InfrastructureStack extends cdk.Stack {
       description: 'Security group for ECS',
     });
     ecsSecurityGroup.addIngressRule(albSecurityGroup, ec2.Port.tcp(8000), 'Allow HTTP traffic from ALB');
+
+    // Create an Elastic Container Registry (ECR) repository
+    const ecrRepository = new ecr.Repository(this, 'matific-django-testapp');    
+
+    // Create an Elastic Container Service (ECS) cluster
+    const ecsCluster = new ecs.Cluster(this, 'TestAppECSCluster', {
+      vpc: vpc,
+      clusterName: 'TestAppECSCluster',
+    });
+
+    // Create an ECS task definition
+    const taskDefinition = new ecs.TaskDefinition(this, 'TestAppDefinition', {
+      compatibility: ecs.Compatibility.FARGATE,
+      cpu: '512',
+      memoryMiB: '1024',
+    });
+
+    // Add a container to the task definition
+    const containerDefinition = taskDefinition.addContainer('TestAppContainer', {
+      image: ecs.ContainerImage.fromEcrRepository(ecrRepository),
+      memoryLimitMiB: 512,
+      environment: {
+        KEY: 'VALUE',
+      },
+    });
+
+    // Create an Application Load Balancer
+    const loadBalancer = new elbv2.ApplicationLoadBalancer(this, 'TestAppLoadBalancer', {
+      vpc: vpc,
+      internetFacing: true,
+      securityGroup: albSecurityGroup,
+    });
+
+    // Create a listener for the load balancer
+    const listener = loadBalancer.addListener('TestAppListener', {
+      port: 80,
+      open: true,
+    });
+
+    // Create a target group for the load balancer
+    const targetGroup = listener.addTargets('TestAppTargetGroup', {
+      targets: [new ecs.LoadBalancerTarget(containerDefinition)],
+      port: 8000,
+      healthCheck: {
+        path: '/',
+      },
+    });
+
+    // Add a service to the ECS cluster
+    const ecsService = new ecs.FargateService(this, 'TestAppECSService', {
+      cluster: ecsCluster,
+      taskDefinition: taskDefinition,
+      desiredCount: 2,
+      serviceName: 'TestAppECSService',
+      securityGroup: ecsSecurityGroup,
+      assignPublicIp: true,
+      platformVersion: ecs.FargatePlatformVersion.VERSION1_4,
+      cloudMapOptions: {
+        name: 'TestAppCloudMapService',
+      },
+    });
+
+    // Add the load balancer to the service
+    ecsService.attachToApplicationTargetGroup(targetGroup);
   }
 }
